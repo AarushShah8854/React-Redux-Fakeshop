bef8d403e4d1aacbe97ecb2dd9fe06282ba1e1ab
AarushShah8854 <aarushshah8854@gmail.com>
2023-12-05 14:48:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductComponent from "./containers/ProductComponent";
+import ProductDetails from "./containers/ProductDetails";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />}/>
+          <Route path="product/:productId" element={<ProductDetails />}/>
+          <Route path="*" element={<h1>No Page Found!!</h1>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/containers/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="ui center aligned header">
+            <h1 className="header">FakeShop</h1>
+        </div>
+    );
+}
+
+export default Header;
